955836c1c7f16ce1b8d8f1551b5500b9df41532a
felfl076 <felfl076@student.liu.se>
2021-04-14 15:05:36 +0200

    [test] Added example code

--- api/test_db.py
+++ api/test_db.py
@@ -1,9 +1,15 @@
 from server import *
 from models import User
 
+
 with app.app_context():
     db.create_all()
-    testy = User(email='admin@example.com')
+    testy = User(email='testy@example.com', first_name='testy', family_name='testo')
     db.session.add(testy)
     db.session.commit()
+    model = User
+    columns = [m.key for m in model.__table__.columns]
+    print(columns)
+    print(model.__table__.columns)
+    print(db.session.query(User).all())
     print(User.query.all())
