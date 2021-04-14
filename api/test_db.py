cca0d66e35c7aa69faa8f1af81964705e0bda84a
felfl076 <felfl076@student.liu.se>
2021-04-14 15:53:49 +0200

    [TEST] Added example code

--- api/test_db.py
+++ api/test_db.py
@@ -1,10 +1,14 @@
 from server import *
 from models import User
 
+# for development purposes because of pycharm...
+from sqlalchemy.orm import Query
+
 
 with app.app_context():
+    query = Query
     db.create_all()
-    testy = User(email='testy@example.com', first_name='testy', family_name='testo')
+    testy = User(email='test@example.com', first_name='testy', family_name='testo')
     db.session.add(testy)
     db.session.commit()
     model = User
@@ -13,3 +17,4 @@ with app.app_context():
     print(model.__table__.columns)
     print(db.session.query(User).all())
     print(User.query.all())
+    print(User.query.filter_by(email='testy@example.com').first())
