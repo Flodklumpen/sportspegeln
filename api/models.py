e3a9e31cada32b3ce8f5f9a2cc7028ff66c9b81e
felfl076 <felfl076@student.liu.se>
2021-04-14 11:48:33 +0200

    [FEAT] All tables are defined

--- api/models.py
+++ api/models.py
@@ -2,6 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 # see https://stackoverflow.com/questions/9692962/flask-sqlalchemy-import-context-issue/9695045#9695045
+# see different relationships here: https://flask-sqlalchemy.palletsprojects.com/en/2.x/models/
 
 
 class User(db.Model):
@@ -11,10 +12,32 @@ class User(db.Model):
 
 
 class Owner(db.Model):
-    email = db.Column(db.String(120), db.ForeignKey('User.email'), nullable=False)
+    email = db.Column(db.String(120), db.ForeignKey(User.email), primary_key=True)
 
 
 class Competitor(db.Model):
-    email = db.Column(db.String(120), db.ForeignKey('User.email'), nullable=False)
+    email = db.Column(db.String(120), db.ForeignKey(User.email), primary_key=True)
 
-# see different relationships here: https://flask-sqlalchemy.palletsprojects.com/en/2.x/models/
+
+class Tournament(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    start_date = db.Column(db.DateTime, nullable=False)
+    end_date = db.Column(db.DateTime)
+    owner = db.Column(db.String(120), db.ForeignKey(Owner.email), nullable=False)
+
+
+class Competing(db.Model):
+    tournament = db.Column(db.Integer, db.ForeignKey(Tournament.id), primary_key=True)
+    competitor = db.Column(db.String(120), db.ForeignKey(Competitor.email), primary_key=True)
+
+
+class Match(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    tournament = db.Column(db.Integer, db.ForeignKey(Tournament.id), primary_key=True)
+    date = db.Column(db.Date)
+    time = db.Column(db.Time)
+    challenger = db.Column(db.String(120), db.ForeignKey(Competitor.email), nullable=False)
+    defender = db.Column(db.String(120), db.ForeignKey(Competitor.email), nullable=False)
+    score_challenger = db.Column(db.Integer)
+    score_defender = db.Column(db.Integer)
