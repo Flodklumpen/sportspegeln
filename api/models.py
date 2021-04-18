f1f295b46a505d0f14f90a5e1c31e52b5bdaa862
felfl076 <felfl076@student.liu.se>
2021-04-18 13:30:47 +0200

    [FEAT][TEST] Database created with tests

--- api/models.py
+++ api/models.py
@@ -1,6 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import ForeignKeyConstraint
-from sqlalchemy.orm import relationship
+
 
 db = SQLAlchemy()
 # see https://stackoverflow.com/questions/9692962/flask-sqlalchemy-import-context-issue/9695045#9695045
@@ -8,39 +7,33 @@ db = SQLAlchemy()
 
 
 class User(db.Model):
-    __tablename__ = 'user'
     email = db.Column(db.String(120), primary_key=True)
     first_name = db.Column(db.String(50), nullable=False)
     family_name = db.Column(db.String(50), nullable=False)
 
 
 class Owner(db.Model):
-    __tablename__ = 'owner'
-    email = db.Column(db.String(120), db.ForeignKey("user.c.email"), primary_key=True)
+    email = db.Column(db.String(120), db.ForeignKey("user.email"), primary_key=True)
+
+
+class Competing(db.Model):
+    competitor = db.Column(db.String(120), db.ForeignKey('competitor.email'), primary_key=True)
+    tournament = db.Column('tourn', db.Integer, db.ForeignKey('tournament.id'), primary_key=True)
 
 
 class Competitor(db.Model):
-    __tablename__ = 'competitor'
     email = db.Column(db.String(120), db.ForeignKey('user.email'), primary_key=True)
 
 
 class Tournament(db.Model):
-    __tablename__ = 'tournament'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     start_date = db.Column(db.DateTime, nullable=False)
     end_date = db.Column(db.DateTime)
-    owner_id = db.Column(db.String(120), db.ForeignKey('owner.email'), nullable=False)
-
-
-competing = db.Table(
-    db.Column('comp', db.String(120), db.ForeignKey('competitor.email'), primary_key=True),
-    db.Column('tour', db.Integer, db.ForeignKey('tournament.id'), primary_key=True)
-)
+    owner = db.Column(db.String(120), db.ForeignKey('owner.email'), nullable=False)
 
 
 class Match(db.Model):
-    __tablename__ = 'match'
     id = db.Column(db.Integer, primary_key=True)
     tournament = db.Column(db.Integer, db.ForeignKey('tournament.id'), primary_key=True)
     date = db.Column(db.Date)
@@ -49,3 +42,16 @@ class Match(db.Model):
     defender = db.Column(db.String(120), db.ForeignKey('competitor.email'), nullable=False)
     score_challenger = db.Column(db.Integer)
     score_defender = db.Column(db.Integer)
+
+
+User.owner = db.relationship("Owner", back_populates="user", uselist=False)
+User.competitor = db.relationship("Competitor", back_populates="user", uselist=False)
+Owner.user = db.relationship("User", uselist=False)
+Competitor.user = db.relationship("User", uselist=False)
+
+"""
+Don't work but for future reference:
+Owner.tournaments = db.relationship("Tournament", order_by=Tournament.owner, back_populates="owner")
+Tournament.owner = db.relationship("Owner", order_by=Owner.email, back_populates="tournaments")
+Competitor.tournaments = db.relationship('Tournament', secondary=tournaments, lazy='subquery', backref=db.backref('competitors', lazy=True))
+"""
---
+++ tests/test_db.py
@@ -0,0 +1,162 @@
+from flask import Flask
+import unittest
+from sqlalchemy import exc
+import datetime
+import sys
+sys.path.append("../api")
+from models import db, User, Owner, Competitor, Tournament, Match, Competing
+
+# for development purposes because of pycharm...
+# from sqlalchemy.orm import Query, query = Query
+
+from sqlalchemy.engine import Engine
+from sqlalchemy import event
+
+
+@event.listens_for(Engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.close()
+
+
+class MainTest(unittest.TestCase):
+
+    TESTING = True
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///:memory"
+    db.init_app(app)
+
+    def setUp(self):
+        with self.app.app_context():
+            db.create_all()
+
+    def tearDown(self):
+        with self.app.app_context():
+            db.drop_all()
+
+
+class AddDataTest(MainTest):
+
+    def test_user(self):
+        with self.app.app_context():
+            db.session.add(User(email='testy1@example.com', first_name='testy1', family_name='testo1'))
+            db.session.commit()
+            user = db.session.query(User).get('testy1@example.com')
+            self.assertIn(user, db.session)
+
+    def test_owner(self):
+        with self.app.app_context():
+            db.session.add(User(email='testy1@example.com', first_name='testy1', family_name='testo1'))
+            db.session.commit()
+            db.session.add(Owner(email='testy1@example.com'))
+            db.session.commit()
+            owner = db.session.query(Owner).get('testy1@example.com')
+            self.assertIn(owner, db.session)
+
+    def test_bad_owner(self):
+        with self.app.app_context():
+            try:
+                db.session.add(Owner(email='testy1@example.com'))
+                return db.session.commit()
+            except exc.IntegrityError:
+                db.session.rollback()
+            owner = db.session.query(Owner).get('testy1@example.com')
+            self.assertIsNone(owner, db.session)
+
+    def test_competitor(self):
+        with self.app.app_context():
+            db.session.add(User(email='testy1@example.com', first_name='testy1', family_name='testo1'))
+            db.session.commit()
+            db.session.add(Competitor(email='testy1@example.com'))
+            db.session.commit()
+            competitor = db.session.query(Competitor).get('testy1@example.com')
+            self.assertIn(competitor, db.session)
+
+    def test_bad_competitor(self):
+        with self.app.app_context():
+            try:
+                db.session.add(Competitor(email='testy1@example.com'))
+                return db.session.commit()
+            except exc.IntegrityError:
+                db.session.rollback()
+            competitor = db.session.query(Competitor).get('testy1@example.com')
+            self.assertIsNone(competitor, db.session)
+
+    def test_tournament(self):
+        with self.app.app_context():
+            db.session.add(User(email='testy1@example.com', first_name='testy1', family_name='testo1'))
+            db.session.commit()
+            db.session.add(Owner(email='testy1@example.com'))
+            db.session.add(Competitor(email='testy1@example.com'))
+            db.session.commit()
+            db.session.add(Tournament(name='testy1´s tournament', start_date=datetime.datetime.now(), owner='testy1@example.com'))
+            db.session.commit()
+            tournament = db.session.query(Tournament).get(1)
+            self.assertIn(tournament, db.session)
+
+    def test_bad_tournament(self):
+        with self.app.app_context():
+            try:
+                db.session.add(Tournament(name='testy1´s tournament', start_date=datetime.datetime.now(), owner='testy1@example.com'))
+                return db.session.commit()
+            except exc.IntegrityError:
+                db.session.rollback()
+            tournament = db.session.query(Tournament).get(1)
+            self.assertIsNone(tournament, db.session)
+
+    def test_competing(self):
+        with self.app.app_context():
+            db.session.add(User(email='testy1@example.com', first_name='testy1', family_name='testo1'))
+            db.session.commit()
+            db.session.add(Owner(email='testy1@example.com'))
+            db.session.add(Competitor(email='testy1@example.com'))
+            db.session.commit()
+            db.session.add(Tournament(name='testy1´s tournament', start_date=datetime.datetime.now(), owner='testy1@example.com'))
+            db.session.commit()
+            db.session.add(Competing(competitor='testy1@example.com', tournament=1))
+            db.session.commit()
+            competing = db.session.query(Competing).get(('testy1@example.com', 1))
+            self.assertIn(competing, db.session)
+
+    def test_bad_competing(self):
+        with self.app.app_context():
+            try:
+                db.session.add(Competing(competitor='testy1@example.com', tournament=1))
+                return db.session.commit()
+            except exc.IntegrityError:
+                db.session.rollback()
+            competing = db.session.query(Competing).get(('testy1@example.com', 1))
+            self.assertIsNone(competing, db.session)
+
+    def test_match(self):
+        with self.app.app_context():
+            db.session.add(User(email='testy1@example.com', first_name='testy1', family_name='testo1'))
+            db.session.add(User(email='testy2@example.com', first_name='testy2', family_name='testo2'))
+            db.session.commit()
+            db.session.add(Owner(email='testy1@example.com'))
+            db.session.add(Competitor(email='testy1@example.com'))
+            db.session.commit()
+            db.session.add(Competitor(email='testy2@example.com'))
+            db.session.commit()
+            db.session.add(Tournament(name='testy1´s tournament', start_date=datetime.datetime.now(), owner='testy1@example.com'))
+            db.session.commit()
+            db.session.add(Match(id=1, tournament=1, challenger='testy1@example.com', defender='testy2@example.com'))
+            db.session.commit()
+            match = db.session.query(Match).get((1, 1))
+            self.assertIn(match, db.session)
+
+    def test_bad_match(self):
+        with self.app.app_context():
+            try:
+                db.session.add(Match(id=1, tournament=1, challenger='testy1@example.com', defender='testy2@example.com'))
+                return db.session.commit()
+            except exc.IntegrityError:
+                db.session.rollback()
+            match = db.session.query(Match).get((1, 1))
+            self.assertIsNone(match, db.session)
+
+
+if __name__ == '__main__':
+    unittest.main()
