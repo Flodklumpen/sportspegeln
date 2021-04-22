6855e00a0501af632853e30e101f73fac0d662f0
Frida Blomstedt <fribl094@student.liu.se>
2021-04-22 16:16:13 +0200

    [FEAT] Finished register user at backend

--- backend/api/routes/login.py
+++ backend/api/routes/login.py
@@ -3,10 +3,27 @@ from ..models.query import *
 
 login_bp = Blueprint('login_bp', __name__)
 
-@login_bp.route('/register')
+def existing_fields(data, fields):
+    return all(elem in data for elem in fields)
+
+
+def filled_fields(data, fields):
+    for field in fields:
+        if not data[field]:
+            return False
+    return True
+
+
+@login_bp.route('/register', methods=['POST'])
 def register():
     data = request.get_json()
     fields = ['given_name', 'family_name', 'email']
+
+    if not existing_fields(data, fields):
+        return jsonify({'message': "Missing field(s)"}), 400
+
+    if not filled_fields(data, fields):
+        return jsonify({'message': "Field(s) not filled"}), 400
 
     if is_user_registered(data['email']):
         return jsonify({'message': "User exists"}), 200
