fc564bc4ee0404878bfd0f347371bc9883fbfa7f
Frida Blomstedt <fribl094@student.liu.se>
2021-04-28 09:04:17 +0200

    [WIP] Added tests to creating match

--- backend/api/routes/match.py
+++ backend/api/routes/match.py
@@ -18,4 +18,22 @@ def filled_fields(data, fields):
 
 @match_bp.route('/create_match', methods=['POST'])
 def create_match():
+    # take in tournament id, date, time, challenger, defender
+    # check if existing and filled fields
+    # check challenger and defender are registered as competing in this tournament
+    # create the match
+    data = request.get_json()
+    fields = ['tournament_id', 'date', 'time', 'challenger', 'defender']
+
+    if not existing_fields(data, required_fields):
+        return jsonify({'message': "Missing field(s)"}), 400
+
+    if not filled_fields(data, required_fields):
+        return jsonify({'message': "Field(s) not filled"}), 400
+
+    if not (query.is_competing(data['challenger'], data['tournament_id']) and query.is_competing(data['defender'], data['tournament_id']):
+        return jsonify({'message': 'Challenger and/or defender are not registered in this tournament'}), 400
+
+
+
     return 200
