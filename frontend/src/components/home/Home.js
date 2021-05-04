d050fc445a5059e780f496c9a8f2ebff88cbdb00
felfl076 <felfl076@student.liu.se>
2021-05-04 18:02:27 +0200

    [REBASE] with master

---
+++ frontend/src/components/home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Tournament } from "../tournament/Tournament";
+
+export function Home() {
+
+	return (
+      <div>
+        <h2>Home</h2>
+        <Tournament />
+      </div>
+    );
+}
+
+export default Home;
