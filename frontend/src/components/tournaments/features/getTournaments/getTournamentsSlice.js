3dd9303b80a44fd38967e8b822e3cf1a69076250
felfl076 <felfl076@student.liu.se>
2021-05-18 15:44:24 +0200

    [FIX] An automatic namechange may have been the cause of tests not working

--- frontend/src/components/tournaments/features/getTournaments/getTournamentsSlice.js
+++ frontend/src/components/tournaments/features/getTournaments/getTournamentsSlice.js
@@ -14,7 +14,7 @@ export default function getTournamentsReducer(state = initialState, action) {
 
 export function fetchTournaments(email, token) {
   return async function fetchTournamentsThunk(dispatch) {
-    const response = await client.get('/tournaments/get_tournaments', email, token);
+    const response = await client.get('/tournament/get_tournaments', email, token);
     dispatch({type: 'data/dataLoaded', payload: response.data});
 
   }
