7a8145e8cd4441b003ade689adc0f8bcd09d8e9c
Frida Blomstedt <fribl094@student.liu.se>
2021-05-30 12:44:37 +0200

    [FIX] Removed unused variable

--- frontend/src/components/ProfileList.js
+++ frontend/src/components/ProfileList.js
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { useAuth0 } from "@auth0/auth0-react";
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import ListGroup from 'react-bootstrap/ListGroup';
